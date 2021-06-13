f0916c0e450a31b378be67f88510f070a4c4e50e
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2021-06-13 11:53:40 -0400

    ABBYYYYYY

--- testImage.py
+++ testImage.py
@@ -1,4 +1,31 @@
-import pyheif
+# module for working with image data
+from PIL import Image
+from matplotlib import image as mpimg
+from matplotlib import pyplot as plt
+import numpy as np
 
-# path to our image
-imagefilePath = r"C:\Users\sebid\Downloads\IMG_0129.heif"
+# filePath for our test picture
+testAbbyFilePath = r"C:\Users\sebid\Downloads\IMG_0129.jpg"
+
+# load our test image
+abby = Image.open(testAbbyFilePath)
+
+# some details about the test pic
+print(abby.format)
+print(abby.mode)
+print(abby.size)
+
+# show abby
+abby.show()
+
+# greyscale abby
+greyscaleAbby = abby.convert(mode="L")
+
+greyscaleAbby.show()
+
+# turn abby into numbers
+abbyArray = asarray(abby)
+
+
+
+
